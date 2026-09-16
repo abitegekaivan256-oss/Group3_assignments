48f476a7a4a1aa0b90ee33bfb66a9117ea00db2e
nsimenta-sandra <nsimentasandra0@gmail.com>
2026-09-16 19:51:59 +0300

    Update weighted_mean.cpp
    contributed by Nsimenta Sandra

--- weighted_mean.cpp
+++ weighted_mean.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Enter number of values: ";
+    cin >> n;
+
+    vector<double> values(n), weights(n);
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter value " << i + 1 << ": ";
+        cin >> values[i];
+        cout << "Enter weight for value " << i + 1 << ": ";
+        cin >> weights[i];
+    }
+
+    double weightedSum = 0.0;
+    double totalWeight = 0.0;
+
+    for (int i = 0; i < n; i++) {
+        weightedSum += values[i] * weights[i];
+        totalWeight += weights[i];
+    }
+
+    if (totalWeight == 0) {
+        cout << "Total weight cannot be zero." << endl;
+        return 1;
+    }
+
+    double weightedMean = weightedSum / totalWeight;
+    cout << "Weighted Mean = " << weightedMean << endl;
+
+    return 0;
+}
